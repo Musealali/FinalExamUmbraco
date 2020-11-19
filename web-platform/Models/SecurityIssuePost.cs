1cdd187144fc69a40e25d78632c5d517ebce5602
thejokerd3 <muslimalali@hotmail.com>
2020-11-19 19:26:27 +0100

    Creating Controller - DbContext - Model

---
+++ web-platform/Models/SecurityIssuePost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_platform.Models
+{
+    public class SecurityIssuePost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public User user { get; set; }
+        public CMSComponent Component { get; set; }
+        public string IssueDescription { get; set; }
+        public string IssueReproduction { get; set; }
+        //TODO public HttpPostedFileBase AttachedFiles { get; set; }
+
+    }
+}

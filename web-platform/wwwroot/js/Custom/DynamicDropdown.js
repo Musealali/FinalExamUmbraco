70c8d23e02b086448ca870050f7f916c7d149c00
dani923y <43599535+dani923y@users.noreply.github.com>
2020-11-21 12:52:23 +0100

    put script into own folder
    added the script to its own folder under

---
+++ web-platform/wwwroot/js/Custom/DynamicDropdown.js
@@ -0,0 +1,23 @@
+﻿function dynamicdropdown(listindex) {
+    const nameSelect = document.getElementById("name")
+    while (nameSelect.options.length > 0) {
+        nameSelect.remove(0);
+    }
+    switch (listindex) {
+        case "cms":
+            nameSelect.options[0] = new Option("Select status");
+            nameSelect.options[0].disabled = true;
+            nameSelect.options[1] = new Option("Umbraco UNO");
+            nameSelect.options[2] = new Option("Umbraco Cloud");
+            nameSelect.options[3] = new Option("Umbraco Hearthbreak");
+            nameSelect.options[4] = new Option("Umbraco CMS");
+            break;
+        case "package":
+            nameSelect.options[0] = new Option("Select status");
+            nameSelect.options[0].disabled = true;
+            nameSelect.options[1] = new Option("Forms");
+            nameSelect.options[2] = new Option("uSync");
+            break;
+    }
+    return true;
+}

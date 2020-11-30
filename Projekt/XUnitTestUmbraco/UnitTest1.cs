845bc1b4b037fa58b65f68e4295ea245dceaf33d
dani923y <43599535+dani923y@users.noreply.github.com>
2020-11-30 15:56:36 +0100

    refactor
    refactor

--- Projekt/XUnitTestUmbraco/UnitTest1.cs
+++ Projekt/XUnitTestUmbraco/UnitTest1.cs
@@ -12,7 +12,7 @@ using web_platform.Models;
 
 namespace XUnitTestUmbraco
 {
-    /*public class Umbraco_FinalExam_UniTest : IDisposable
+    public class Umbraco_FinalExam_UniTest : IDisposable
     {
         protected readonly UmbracoDbContext _context;
         private SecurityIssuePostController _controller;
@@ -118,5 +118,5 @@ namespace XUnitTestUmbraco
             var objectResult = Assert.IsAssignableFrom<ActionResult>(result);
         }
 
-    }*/
+    }
 }

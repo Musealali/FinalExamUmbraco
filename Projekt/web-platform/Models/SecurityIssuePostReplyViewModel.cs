e1a3944d10f1f294fbfe670f0e2abfdf3200b62a
thejokerd3 <muslimalali@hotmail.com>
2020-12-03 14:14:42 +0100

    Validation to Security Issue Reply Content

--- Projekt/web-platform/Models/SecurityIssuePostReplyViewModel.cs
+++ Projekt/web-platform/Models/SecurityIssuePostReplyViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using web_platform.Data.Models;
@@ -9,6 +10,7 @@ namespace web_platform.Models
     public class SecurityIssuePostReplyViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string Content { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
     }

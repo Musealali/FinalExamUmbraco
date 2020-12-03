823c150af53dfea26189905f1b542573801678cb
Ricki <ricki_ricko2@hotmail.com>
2020-12-03 10:18:58 +0100

    If dev env, create default user in db and view

---
+++ Projekt/web-platform.Data/IdentityInitializer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using web_platform.Data.Models;
+
+namespace web_platform.Data
+{
+    public class IdentityInitializer
+    {
+        public static void InitializeDevelopment(IServiceProvider services)
+        {
+            using (var scope = services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                UserManager<ApplicationUser> userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                SignInManager<ApplicationUser> signInManager = scope.ServiceProvider.GetRequiredService<SignInManager<ApplicationUser>>();
+
+                var user = new ApplicationUser() { UserName = "default@default.com", Email = "default@default.com" };
+                var createResult = userManager.CreateAsync(user, "Default1!").Result;
+            }
+        }
+    }
+}

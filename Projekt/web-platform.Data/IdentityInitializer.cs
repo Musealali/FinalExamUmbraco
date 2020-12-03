b4e5d139be023f536b63471e3dc4da64afd8c7aa
Ricki <ricki_ricko2@hotmail.com>
2020-12-03 15:19:50 +0100

    Added admin role, and set default user to admin

--- Projekt/web-platform.Data/IdentityInitializer.cs
+++ Projekt/web-platform.Data/IdentityInitializer.cs
@@ -13,10 +13,28 @@ namespace web_platform.Data
         {
             using (var scope = services.GetRequiredService<IServiceScopeFactory>().CreateScope())
             {
+                RoleManager<ApplicationRole> roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+                ApplicationRole adminRole = new ApplicationRole("Administrator");
+
+                if (!roleManager.RoleExistsAsync("Administrator").Result)
+                {
+                    _ = roleManager.CreateAsync(adminRole).Result;
+                }
+
+
                 UserManager<ApplicationUser> userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
-                var user = new ApplicationUser() { UserName = "default@default.com", Email = "default@default.com" };
-                var createResult = userManager.CreateAsync(user, "Default1!").Result;
+                var user = userManager.FindByEmailAsync("default@default.com").Result;
+                if(user == null)
+                {
+                    user = new ApplicationUser() { UserName = "default@default.com", Email = "default@default.com" };
+                    var createResult = userManager.CreateAsync(user, "Default1!").Result;
+                }
+
+                if(!userManager.IsInRoleAsync(user, adminRole.Name).Result)
+                {
+                    var roleAddResult = userManager.AddToRoleAsync(user, adminRole.Name).Result;
+                }
             }
         }
     }

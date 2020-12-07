6994a3b9a358d2264741095c12b9bcfc7618d18c
dani923y <43599535+dani923y@users.noreply.github.com>
2020-12-07 15:54:23 +0100

    removed the mothod call from index
    removed the method call from index as it was only used to debug the method

--- Projekt/web-platform.Data/APIConnecter.cs
+++ Projekt/web-platform.Data/APIConnecter.cs
@@ -15,17 +15,14 @@ namespace web_platform.Data
     {
         public HttpClient Client { get; set; } = new HttpClient();
 
-        public async Task<string> GetPackages()
+        public async Task<List<Package>> GetPackages()
         {
             var result = await Client.GetStringAsync("https://our.umbraco.com/webapi/packages/v1?pageIndex=0&pageSize=10000&category=&query=testing&order=Default%22");
             XmlDocument doc = new XmlDocument();
             doc.LoadXml(result);
             var json = JsonConvert.SerializeXmlNode(doc);
             var data = JsonConvert.DeserializeObject<MyJsonType>(json);
-            //var myJObject = JObject.Parse(json);
-            //var myList = myJObject.SelectTokens("$.PagedPackages.Packages.Package").Value<JObject>().ToList();
-            Console.WriteLine("test");
-            return json;
+            return data.PagedPackages.Packages.Package;
         }
     }
 }

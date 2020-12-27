1569910360256873fc82303edaaa515c82e9af9f
Rick0335 <Rick0335@edu.eal.dk>
2020-12-27 03:17:34 +0100

    grouped methods by usage

--- Projekt/web-platform.Data/ISecurityIssuePost.cs
+++ Projekt/web-platform.Data/ISecurityIssuePost.cs
@@ -10,22 +10,19 @@ namespace web_platform.Data
     public interface ISecurityIssuePost
     {
         Task<SecurityIssuePost> GetById(int id);
-
-        Task<SecurityIssuePost> CreateSecurityIssuePost(string title, string issueDescription, string componentName, string componentVersion, ApplicationUser applicationUser);
-        Task<SecurityIssuePostReply> CreateSecurityIssuePostReply(string content, SecurityIssuePost securityIssuePost, ApplicationUser applicationUser);
-        Task<SecurityIssuePostReply> GetSecurityIssuePostReply(int securityIssuePostReplyId);
-
-        Task<List<SecurityIssuePost>> GetSecurityIssuePostsByState(State state);
-        Task<List<SecurityIssuePostReply>> GetSecurityIssuePostsReplies(int securityIssuePostId);
-        Task DeleteSecurityIssuePostReply(int securityIssuePostReplyId);
-
-        State GetSecurityIssuePostStateVerified();
-
-        State GetSecurityIssuePostStateNotVerified();
         Task<List<SecurityIssuePost>> GetSecurityIssuePostsBySearchString(string searchString, State state);
-        Task<SecurityIssuePost> ChangeSecurityIssuePostStateToVerified(int securityIssuePostId);
+        Task<SecurityIssuePost> CreateSecurityIssuePost(string title, string issueDescription, string componentName, string componentVersion, ApplicationUser applicationUser);
+        Task<List<SecurityIssuePost>> GetSecurityIssuePostsByState(State state);
         Task DeleteSecurityIssuePost(int securityIssuePostId);
+        Task<SecurityIssuePost> ChangeSecurityIssuePostStateToVerified(int securityIssuePostId);
         Task<SecurityIssuePost> UpdateSecurityIssuePost(int securityIssuePostId, string title, string issueDescription, string componentName, string componentVersion);
 
+        State GetSecurityIssuePostStateVerified();
+        State GetSecurityIssuePostStateNotVerified();
+
+        Task<SecurityIssuePostReply> GetSecurityIssuePostReply(int securityIssuePostReplyId);
+        Task<List<SecurityIssuePostReply>> GetSecurityIssuePostsReplies(int securityIssuePostId);
+        Task<SecurityIssuePostReply> CreateSecurityIssuePostReply(string content, SecurityIssuePost securityIssuePost, ApplicationUser applicationUser);
+        Task DeleteSecurityIssuePostReply(int securityIssuePostReplyId);
     }
 }
